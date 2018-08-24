1562ec6a802a68a8aaccce91b6f50b40010b64d9
tonymthediggity <35578106+tonymthediggity@users.noreply.github.com>
2018-08-23 21:52:25 -0500

    Initial commit

---
+++ Assets/BaseHP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHP : MonoBehaviour {
+
+    public float baseHP;
+    public GameObject enemy;
+    public EnemyAI enemyDamage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyDamage = enemy.GetComponent<EnemyAI>();
+
+
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Doing Damage");
+            baseHP -= enemyDamage.damage;
+        }
+    }
+
+  
+}
---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    public Rigidbody myBody;
+    public Vector3 basePos;
+    public GameObject playerBase;
+
+    public float damage;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        myBody = GetComponent<Rigidbody>();
+        playerBase = GameObject.FindGameObjectWithTag("Base");
+        basePos = playerBase.transform.position;
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.LookAt(basePos);
+        myBody.AddForce(transform.forward * speed);
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Base"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
